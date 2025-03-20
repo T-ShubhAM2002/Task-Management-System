0fa93e12091b4760c1069d7ddd95588a0a40d6bd
T Shubham <shubham2003march25@gmail.com>
2025-03-20 08:44:41 +0530

    final changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4001/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4001/api";
 
 // Create axios instance with base URL
 const api = axios.create({
